5c7f0209d5c36a07bc7039e3b15d36748522548c
Lucas Latsch <bello-lucas@hotmail.com>
2025-04-01 21:41:09 -0300

    Add DishCard and Dishes components to display menu items

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import HeroSection from "./components/HeroSection/HeroSection";
+import Navbar from "./components/Navbar/Navbar";
+import Dishes from "./components/Dishes/Dishes";
+import { About } from "./components/About/About";
 
 const App = () => {
   return (
-    <div>
+    <main className="overflow-y-hidden text-neutral-200 antialiased">
       <HeroSection />
-    </div>
+      <Navbar />
+      <Dishes />
+      <About />
+    </main>
   );
 };
 
