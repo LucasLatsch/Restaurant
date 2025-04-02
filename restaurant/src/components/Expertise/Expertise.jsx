b8958fb09c66c00709d3c3d3e7231762f3299406
Lucas Latsch <bello-lucas@hotmail.com>
2025-04-01 23:49:57 -0300

    Add framer-motion for animations in various components

--- restaurant/src/components/Expertise/Expertise.jsx
+++ restaurant/src/components/Expertise/Expertise.jsx
@@ -1,4 +1,26 @@
 import { CUSINES } from "../../constants";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
 
 const Expertise = () => {
   return (
@@ -6,9 +28,17 @@ const Expertise = () => {
       <h2 className="my-8 text-center text-3xl tracking-tighter lg:text-4xl">
         Our Expertise
       </h2>
-      <div className="container mx-auto px-4">
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={containerVariants}
+        className="container mx-auto px-4"
+      >
         {CUSINES.map((cusine, index) => (
-          <div
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={itemVariants}
             key={index}
             className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2"
           >
@@ -28,9 +58,9 @@ const Expertise = () => {
                 </p>
               </h3>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
--- restaurant/src/components/Review/Review.jsx
+++ restaurant/src/components/Review/Review.jsx
@@ -5,15 +5,51 @@ import customer1 from "../../assets/customer1.jpeg";
 import customer2 from "../../assets/customer2.jpeg";
 import customer3 from "../../assets/customer3.jpeg";
 import customer4 from "../../assets/customer4.jpeg";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.8,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 const Review = () => {
   return (
     <section id="review" className="container mx-auto mb8 mt-12">
-      <div className="flex flex-col">
-        <p className="mv-10 text-3xl font-light leading-normal tracking-tighter p-4 lg:mx-40 lg:mt-40 lg:text-[3.5rem]">
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={containerVariants}
+        viewport={{ once: true }}
+        className="flex flex-col"
+      >
+        <motion.p
+          variants={itemVariants}
+          viewport={{ once: true }}
+          className="mv-10 text-3xl font-light leading-normal tracking-tighter p-4 lg:mx-40 lg:mt-40 lg:text-[3.5rem]"
+        >
           {REVIEW.content}
-        </p>
-        <div className="flex items-center justify-center gap-6">
+        </motion.p>
+        <motion.div
+          variants={itemVariants}
+          viewport={{ once: true }}
+          className="flex items-center justify-center gap-6"
+        >
           <img
             src={xaviour}
             width={80}
@@ -25,18 +61,25 @@ const Review = () => {
             <h6>{REVIEW.name}</h6>
             <p className="text-sm text-neutral-500">{REVIEW.profession}</p>
           </div>
-        </div>
-      </div>
-      <div className="mt-14 flex flex-col items-center justify-center gap-2 md:flex-row">
+        </motion.div>
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={itemVariants}
+        viewport={{ once: true }}
+        className="mt-14 flex flex-col items-center justify-center gap-2 md:flex-row"
+      >
         {[customer1, customer2, customer3, customer4].map((customer, index) => (
-          <img
+          <motion.img
+            variants={itemVariants}
             key={index}
             src={customer}
             alt="customer"
             className="h-[300px] w-[200px] rouded-tl-3xl object-cover"
-          ></img>
+          ></motion.img>
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 };
